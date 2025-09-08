e7d454f460c8e342382797aa4db7b18cf2449727
sandrasavkovic <sandrasavkovic86@gmail.com>
2025-09-09 00:45:54 +0200

    Initial commit

---
+++ MyProjectBackendSolution/MyProjectBackend/Program.cs
@@ -0,0 +1,69 @@
+/*
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MyProjectBackend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProjectBackend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+*/
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+     policy =>
+     {
+         policy.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+     });
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ MyProjectBackendSolution/MyProjectBackend/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MyProjectBackend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // podesavanje dependency injection obrasca
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyProjectBackend", Version = "v1" });
+            });
+
+            /*
+            //Dodavanje Race servisa i njegovog interfejsa u kontejner zavisnosti, prvo navodimo interfejs a zatim njegovu implementaciju
+            services.AddScoped<IScopedRaceService, RaceService>(); //Instanca servisa je ista u okviru jednog HTTP zahteva
+            services.AddScoped<IScopedGetterService, GetterService>(); //Instanca servisa je ista u okviru jednog HTTP zahteva
+            services.AddSingleton<ISingletonRaceService, RaceService>(); //Instanca servisa je ista uvek
+            services.AddTransient<ITransientRaceService, RaceService>(); //Instanca servisa je razlicita uvek, cak i ukviru istog zahteva
+            services.AddTransient<ITransientGetterService, GetterService>(); //Instanca servisa je razlicita uvek, cak i ukviru istog zahteva
+        */
+            }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // bitan redosled!!!
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyProjectBackend"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
